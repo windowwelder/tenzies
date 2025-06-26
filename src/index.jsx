29ed0b71ab13b60ac2566d6a19cfb0d23e3ae541
windowwelder <welderules@duck.com>
2025-06-26 20:00:44 +0700

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
+
+import React from 'react';
+import './index.css';
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h1>Hello, world!</h1>
+  </React.StrictMode>
+);
