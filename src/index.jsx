e7117343139c60a01f60757c95e31778b1bd8314
windowwelder <welderules@duck.com>
2025-06-27 11:35:28 +0700

    feat: creating App component and rendering it in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,12 +8,9 @@
 
 import React from 'react';
 import './index.css';
-
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <h1>Hello, world!</h1>
-  </React.StrictMode>
+  <App />
 );
