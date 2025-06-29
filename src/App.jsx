59844afe4c5074b1204f5edfc58addd75836a82c
windowwelder <welderules@duck.com>
2025-06-30 04:55:42 +0700

    feat: changing array of numbers to an array of objects

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import { useState } from "react"
 
 export default function App() {
         const generateAllDice = () => {
-        return [...Array(10)].map( () => Math.ceil(Math.random()*6) )
+        return [...Array(10)].map( () =>
+                ({
+                    value: Math.ceil(Math.random() * 6),
+                    isHeld: false
+                })
+                    )
         }
+
         const [dice, setDice] = useState(generateAllDice())
                 
-        const diceElements = dice.map( num  => <Die value={num} />)
+        const diceElements = dice.map(dice => <Die value={dice.value} />)
 
         const updateAllDice = () => {
         setDice(generateAllDice())
