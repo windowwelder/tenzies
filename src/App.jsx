6f69eb76ddaa5effb1c5eaadfcec67e7916dabbc
windowwelder <welderules@duck.com>
2025-06-30 04:17:03 +0700

    feat: creating Roll button and fixing problem with generateAllDice

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import Die from "./Die"
 import { useState } from "react"
 
 export default function App() {
-        const [dice, setDice] = useState(generateAllDice())
-        
         const generateAllDice = () => {
         return [...Array(10)].map( () => Math.ceil(Math.random()*6) )
         }
-        
+        const [dice, setDice] = useState(generateAllDice())
+                
         const diceElements = dice.map( num  => <Die value={num} />)
+
+        const updateAllDice = () => {
+        setDice(generateAllDice())
+        }
 
     return (
         <main>
             <div className="container">
                 {diceElements}
             </div>
+            <button onClick={updateAllDice}>Roll</button>
         </main>
     )
 }
