789ceb55c04a4b500cf36f81e86812db6ab12501
windowwelder <welderules@duck.com>
2025-06-30 13:37:01 +0700

    style: adding and styling the title

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,8 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="container">
                 {diceElements}
             </div>
