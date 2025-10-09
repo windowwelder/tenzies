2ba1fbe97a2f477e44c0b629188b9278a0dbff8d
windowwelder <welderules@duck.com>
2025-10-09 14:38:59 +0700

    fix: vercel's problem with images not in public folder

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,14 +1,10 @@
 export default function Die(props) {
-    //
-    // 1)add the image with condition:
-    //  if value is 1 then image is die-1 (with template literals)
-    // 2) add condition with props.isHeld
 
-    const src=`./src/assets/die${props.isHeld ? "-held": ""}-${props.value}.svg`
-    
+    const src=`./dice/die${props.isHeld ? "-held": ""}-${props.value}.svg`
+
     return (<button 
                 onClick={ () => props.hold()}
                 style={{ backgroundImage: `url(${src})` }}
-            >
+                >
             </button>)
 }
